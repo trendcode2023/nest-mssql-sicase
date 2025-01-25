01248b3414f832015827c8dc3f245ef8914c7258
johanrocha <johan.rocha.horna@gmail.com>
2025-01-24 21:46:16 -0500

    instalacion de @nestjs/swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LoggerGlobal } from './middlewares/middleware.logger';
-
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(LoggerGlobal);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('DemoNest')
+    .setDescription(
+      'esta es una api construida para ser empleada en el modulo 4',
+    )
+    .setVersion('1.0')
+    //.addTag('users') // agregar tag a la documentacion
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 3000;
 
   await app.listen(port);
