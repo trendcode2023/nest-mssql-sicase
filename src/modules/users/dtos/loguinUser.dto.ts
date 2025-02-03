3dd0e6a60ed7414f246bd0fdc7fe62b54f67e525
Frank Rocha <frankalessandror@gmail.com>
2025-02-02 19:48:10 -0500

    fix: nullable mfacode

--- src/modules/users/dtos/loguinUser.dto.ts
+++ src/modules/users/dtos/loguinUser.dto.ts
@@ -1,11 +1,12 @@
 import { PickType } from '@nestjs/swagger';
 import { CreateUserDto } from '../../users/dtos/createUser.dto';
-import { IsString } from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
 export class LoguinUserDto extends PickType(CreateUserDto, [
   'email',
   'password',
 ]) {
+  @IsOptional()
   @IsString()
-  mfaCode:string
+  mfaCode: string | null; 
 }
